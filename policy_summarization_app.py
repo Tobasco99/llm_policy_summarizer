e603b296b8d94b3c5e22c7bbdc71e6f3b5ef50e7
Tobasco99 <kiel.tobi99@googlemail.com>
2024-01-30 18:40:01 +0100

    first basic summarizer with pypdf loader

--- policy_summarization_app.py
+++ policy_summarization_app.py
@@ -1,22 +1,76 @@
 import streamlit as st
 import openai
 import os
+from tempfile import NamedTemporaryFile
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.chat_models import ChatOpenAI
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
+#openai.api_key = os.environ["OPENAI_API_KEY"]
 
-def custom_summary(docs,llm, custom_prompt, chain_type):
-    custom_prompt = custom_prompt + """:\n {text}"""
-    COMBINE_PROMPT = PromptTemplate(template=custom_prompt, input_variables = ["text"])
-    MAP_PROMPT = PromptTemplate(template="Summarize:\n{text}", input_variables=["text"])
+map_template = """The following is a set of documents from a political policy:
+
+{text}
+
+Based on this list of documents, please summarize the key topics and decisions that were made.
+SUMMARY:"""
+
+combine_template = """The following is a set of summaries from a political policy:
+
+{text}
+
+Take these and distill it into a comprehensive summary for a person {knowledge} domain knowledge of the 
+main topic. If present especially focus on the following topic: {focus}
+If no topic is provided, just write a comprehensive summary including all topics.
+SUMMARY:"""
+
+refine_template = """Write a comprehensive summary of this video transcript. 
+
+Divide it into,
+1. Dopamine and Procrastination
+2. Tools proposed
+3. Protocols
+4. Conclusion
+
+{text}
+
+SUMMARY:"""
+
+def save_uploaded_file(uploaded_file):
+    """
+    Saves the uploaded file to a temporary location on the server.
+
+    Args:
+    uploaded_file (BytesIO): The uploaded file object.
+
+    Returns:
+    str: The file path where the uploaded file is saved.
+    """
+    with NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(uploaded_file.read())
+        file_path = temp_file.name
+    return file_path
+
+def delete_file(file_path):
+    """
+    Deletes the file from the server.
+
+    Args:
+    file_path (str): The path of the file to be deleted.
+    """
+    os.remove(file_path)
+
+def custom_summary(docs,llm, user_input, chain_type, domain_knowledge, temperature):
+    COMBINE_PROMPT = PromptTemplate(template=combine_template, input_variables=["text"],
+                                                partial_variables={"focus": user_input ,"knowledge": "with" if domain_knowledge else "without"})
+    MAP_PROMPT = PromptTemplate(template=map_template, input_variables=["text"])
     if chain_type == "map_reduce":
         chain = load_summarize_chain(llm,chain_type=chain_type,
                                      map_prompt=MAP_PROMPT,
-                                     combine_prompt=COMBINE_PROMPT)
+                                     combine_prompt=COMBINE_PROMPT,
+                                     temperature=temperature)
     else:
         chain = load_summarize_chain(llm,chain_type=chain_type)
     
@@ -25,14 +79,16 @@ def custom_summary(docs,llm, custom_prompt, chain_type):
     return summary_output
 
 @st.cache_data
-def setup_documents(pdf_file_path, chunk_size, chunk_overlap):
-    loader = PyPDFLoader(pdf_file_path)
+def setup_documents(uploaded_file, chunk_size, chunk_overlap):
+    file_path = save_uploaded_file(uploaded_file)
+    loader = PyPDFLoader(file_path)
     docs_raw = loader.load()
     docs_raw_text = [doc.page_content for doc in docs_raw]
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,
                                                    chunk_overlap=chunk_overlap)
     docs = text_splitter.create_documents(docs_raw_text)
-    
+    # Delete the file after use
+    delete_file(file_path)
     return docs
 
 def main():
@@ -58,7 +114,7 @@ def main():
             user_prompt = st.text_input("Enter custom summarization requests")
             if uploaded_file is not None:
                 df = "file"
-            on = st.toggle('Domain knowledge?')
+            knowledge = st.toggle('Domain knowledge?')
 
             temperature = st.sidebar.number_input("LLM Temperature", min_value=0.0, max_value=1.0, step=0.1, value=0.0)
             
@@ -75,7 +131,7 @@ def main():
                 st.write("Policy was loaded successfully")
                 
                 if st.button("Summarize"):
-                    result = custom_summary(docs,llm, user_prompt, chain_type)
+                    result = custom_summary(docs,llm, user_prompt, chain_type, knowledge, temperature)
                     st.write("Summary:")
                     st.info(result)
                     if st.button("Download"):
