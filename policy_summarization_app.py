d39a737df897ea1c5b4e4a270c75240527779f33
Tobasco99 <kiel.tobi99@googlemail.com>
2024-01-30 15:11:32 +0100

    add logic

--- policy_summarization_app.py
+++ policy_summarization_app.py
@@ -7,12 +7,33 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.chat_models import ChatOpenAI
 
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
 def custom_summary(docs,llm, custom_prompt, chain_type):
-    return "This is a generated Summary"
+    custom_prompt = custom_prompt + """:\n {text}"""
+    COMBINE_PROMPT = PromptTemplate(template=custom_prompt, input_variables = ["text"])
+    MAP_PROMPT = PromptTemplate(template="Summarize:\n{text}", input_variables=["text"])
+    if chain_type == "map_reduce":
+        chain = load_summarize_chain(llm,chain_type=chain_type,
+                                     map_prompt=MAP_PROMPT,
+                                     combine_prompt=COMBINE_PROMPT)
+    else:
+        chain = load_summarize_chain(llm,chain_type=chain_type)
+    
+    summary_output = chain({"input_documents": docs}, return_only_outputs=True)["output_text"]
+    
+    return summary_output
 
 @st.cache_data
 def setup_documents(pdf_file_path, chunk_size, chunk_overlap):
-    pass
+    loader = PyPDFLoader(pdf_file_path)
+    docs_raw = loader.load()
+    docs_raw_text = [doc.page_content for doc in docs_raw]
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,
+                                                   chunk_overlap=chunk_overlap)
+    docs = text_splitter.create_documents(docs_raw_text)
+    
+    return docs
 
 def main():
     st.set_page_config(layout="wide")
