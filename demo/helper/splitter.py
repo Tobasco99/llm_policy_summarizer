57bd80d4b6e7421eca2d63b9369fe60973d9c555
Tobasco99 <kiel.tobi99@googlemail.com>
2024-04-03 09:20:27 +0300

    weaviate changes

--- demo/helper/splitter.py
+++ demo/helper/splitter.py
@@ -2,7 +2,7 @@ from langchain_text_splitters import (
     Language,
     RecursiveCharacterTextSplitter,
 )
-from typing import List
+from typing import list
 import os
 from tempfile import NamedTemporaryFile
 import sys
@@ -16,35 +16,33 @@ import weaviate.classes as wvc
 from weaviate.util import generate_uuid5
 
 
-def __store_embeddings(embeddings:List):
+def __store_embeddings(embeddings:list, chunks:list, title:str):
+    # get from env
     client = weaviate.connect_to_custom(http_host="weaviate-mahk.tech4comp.dbis.rwth-aachen.de", http_secure=True, 
                                         auth_credentials=weaviate.classes.init.Auth.bearer_token(access_token="f5b035cd-548f-441d-adc8-6029fce22a2b"))
-
     try:
         props = []
-        uuids = []
-        for embedding in embeddings:
-            object_props = {"chunk_embedding": embedding}
-            object_uuid = generate_uuid5(object_props)
+        for chunk in chunks:
+            object_props = {"chunk": chunk,
+                            "doc_title": title}
             props.append(object_props)
-            uuids.append(object_uuid)
 
         with client.batch.dynamic(  # client.batch.dynamic() or client.batch.rate_limit() also possible
             consistency_level=wvc.ConsistencyLevel.QUORUM
         ) as batch:
             # Add objects to the batch, e.g.
-            for i in range(len(props)):
+            for i, property in enumerate(props):
                 batch.add_object(
                     collection="policies",
-                    properties=props[i],
-                    uuid=uuids[i],
+                    properties=property,
+                    vector = embeddings[i],
                     # tenant="tenantA"  # Optional; specify the tenant in multi-tenancy collections
                 )
 
     finally:
         client.close()
 
-def __save_uploaded_file(uploaded_file):
+def __save_uploaded_file(uploaded_file:bytes):
     """
     Saves the uploaded file to a temporary location on the server.
 
@@ -63,7 +61,7 @@ def __save_uploaded_file(uploaded_file):
         file_path = temp_file.name
     return file_path
 
-def __delete_file(file_path):
+def __delete_file(file_path:str):
     """
     Deletes the file from the server.
 
@@ -73,7 +71,7 @@ def __delete_file(file_path):
     """
     os.remove(file_path)
 
-def __get_html_splitter(chunk_size, chunk_overlap):
+def __get_html_splitter(chunk_size:int, chunk_overlap:int):
     splitter = RecursiveCharacterTextSplitter.from_language(
         language=Language.HTML, chunk_size=chunk_size, chunk_overlap=chunk_overlap
     )
@@ -88,7 +86,7 @@ def __get_sentence_splitter():
     tag_splitter = XMLSentenceSplitter(tags_to_split_on=tags_to_split_on)
     return tag_splitter
 
-def __get_docs_from_structure_splitter(chunk_size, file_path):
+def __get_docs_from_structure_splitter(chunk_size:int, file_path:str):
 
     text_splitter = XMLTagTextSplitter(first_tag="div", second_tag="p", max_chunk_size=chunk_size)
 
@@ -100,7 +98,7 @@ def __get_docs_from_structure_splitter(chunk_size, file_path):
     return docs
 
 
-def load_and_split_text(text, chunk_size, chunk_overlap, splitter_type):
+def load_and_split_text(text:str, chunk_size:int, chunk_overlap:int, splitter_type:str):
     """Retrieve the file splitted in documents with given parameters.
 
     Parameters
@@ -134,19 +132,21 @@ def load_and_split_text(text, chunk_size, chunk_overlap, splitter_type):
 
     return docs
 
-def vectorize_docs(docs: List, vectorizer: str, key:str):
+def vectorize_docs(docs:list, vectorizer:str, key:str, title:str = "dummy"):
     """Retrieve the file splitted in documents with given parameters.
 
     Parameters
     ----------
-    docs (List[str]): The list of document chunks
+    docs (List[str]): The list of document chunks.
     vectorizer (str): The intended vectorizer to use.
-    key(str): the OpenAI key.
+    key(str): The OpenAI key.
+    title(str): The document title.
 
     Returns
     -------
     List[number]: the vectorized chunks.
     """
+    # get from env
     url = 'http://137.226.232.15:11434/api/embeddings'
 
     if vectorizer == "OpenAI Embeddings":
@@ -169,7 +169,7 @@ def vectorize_docs(docs: List, vectorizer: str, key:str):
         embedding = requests.post(url, json = body)
 
     # send embeddings to database
-    #__store_embeddings(embedding)
+    #__store_embeddings(embedding, docs, title)
 
     # later return none  
     return embedding
