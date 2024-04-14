41fef189dae87eaabcf07344af0cf39c06fea772
Tobasco99 <kiel.tobi99@googlemail.com>
2024-04-14 13:29:55 +0200

    add first prompt

--- demo/helper/prompt.py
+++ demo/helper/prompt.py
@@ -25,6 +25,30 @@ def get_prompt_map_chunk(text_chunk):
                 - Finally, use the key information and the stakeholder to generate a summary of the text and write it in the chunk_summary key.
                 """)
 
-    prompt += f"{text_chunk}"
+    prompt += f"<{text_chunk}>"
+
+    return prompt
+
+def get_prompt_reduce(map_jsons):
+    """
+    Generate a prompt summarizing with the result of map step.
+
+    Args:
+        map_jsons list(str): the output of the map step as a json string.
+
+    Returns:
+        str: The generated prompt.
+    """
+    prompt = dedent("""\
+                Given the information of a policy document as JSONs, generate a consicive summary. 
+                Format your response as JSON with the following structure:
+                To effectively complete the summarization, follow these steps:
+                - First, make sure you name the stakeholders involved with the stakeholder key.
+                - Second, make sure that all key information from the key_information key are involved.
+                - Finally, use the intermediate summaries and the information you have to generate a concise summary of the document.
+                These are the JSONs:
+                """)
+
+    prompt += f"{map_jsons}"
 
     return prompt
