a358e9c1bbc5fc8f7fded72a845c8e18cb280737
Tobasco99 <kiel.tobi99@googlemail.com>
2024-04-27 16:29:41 +0200

    move logic to class

--- app/service/language_model_connection.py
+++ app/service/language_model_connection.py
@@ -2,6 +2,11 @@ from enum import Enum
 from dotenv import load_dotenv, find_dotenv
 import requests
 import os
+from langchain_core.output_parsers import JsonOutputParser
+from typing import List
+from langchain_core.prompts import PromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_openai import OpenAI
 
 
 class LanguageModel(Enum):
@@ -9,7 +14,15 @@ class LanguageModel(Enum):
     GPT_3_5 = "gpt-3.5-turbo"
     GPT_4 = "gpt-4-turbo"
 
-class languageModelConnection:
+class Question(BaseModel):
+    Question: str = Field(description="question to check the knowledge level")
+    answers: list = Field(description="list of 4 possible answers, only one is correct, include A, B, C, D before question")
+    correct_answer: str = Field(description="correct answer")
+
+class Questions(BaseModel):
+    Questions: List[Question] = Field(description="question object")
+
+class LanguageModelConnection:
     def __init__(self, model:LanguageModel, key:str|None = None):
         self.model = model
         self.key = key
@@ -18,13 +31,30 @@ class languageModelConnection:
                 load_dotenv(find_dotenv())
                 self.ol_url = os.environ.get("OLLAMA_URL")
                 self.model_url = self.ol_url+'/api/generate'
+        elif model == LanguageModel.GPT_3_5:
+                self.model_url = 'https://api.openai.com/v1/engines/gpt-3.5-turbo/completions'
+        elif model == LanguageModel.GPT_4:  
+                self.model_url = 'https://api.openai.com/v1/engines/gpt-4-turbo/completions'
 
-    def generate_abstract(self, text):
-        if self.model == LanguageModel.LLAMA2:
-            body = {'model': 'llama2',
-                    'prompt': f"Give me a summary of the key regulations described in this policy document:{text}",
-                    'stream': False}  
-            response = requests.post(self.model_url, json=body)
-            abstract = response.json()["response"]
-            return abstract
+    def generate_questionnaire(self, topic):
+        # A query intented to prompt a language model to populate the data structure.
+        question_query = "Your job is to determine the level of knowledge of a user. Given a topic ask 2 multiple choice questions with 4 possible answers each, that help to determine the knowledge level."
 
+        # Set up a parser + inject instructions into the prompt template.
+        parser = JsonOutputParser(pydantic_object=Questions)
+
+        prompt = PromptTemplate(
+        template="The topic is {topic}.\n{format_instructions}\n{query}\n",
+        input_variables=["query", "topic"],
+        partial_variables={"format_instructions": parser.get_format_instructions()},
+        )
+
+        if self.model == LanguageModel.GPT_3_5:
+                llm = OpenAI(openai_api_key=self.key)
+        else:
+                llm = OpenAI(openai_api_key=self.key)
+
+        chain = prompt | llm | parser
+
+        result = chain.invoke({"query": question_query, "topic": topic})
+        return result
